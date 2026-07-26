abae9dabb30b20855747b150e8a4b69e53c89b86
Omega <164745966+AnuragKumar903@users.noreply.github.com>
2026-07-26 19:48:21 +0530

    Time: 59 ms (19.54%), Space: 67.2 MB (7.8%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int, int> freq;
+        for(int x : nums){
+            freq[x]++;
+        }
+        for(auto it : freq){
+            if(it.second == 2)  ans.push_back(it.first);
+        }
+
+        return ans;
+    }
+};
